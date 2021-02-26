d971b52a25bfefef765a6bc60ac2799e27f9a7a4
IT-Support-L2 <itech@cyberservices.com>
2021-02-27 00:22:42 +0100

    Update code.py

--- code.py
+++ code.py
@@ -44,10 +44,40 @@ with open("file.txt", 'r') as f:
             l.append(word)
    print(l)
 
-# shortest frequency dictionary code
+# shortest frequency dictionary count
 
 s = 'Python is fun'
 d = {}
 for i in s:
     d[i] = d.get(i, 0) + 1
 print(d)
+
+
+# useful lambda, list comprehension, zip and filter examples
+
+L1 = [1, 5, 2, 16, 32, 3, 54, 8, 100]
+L2 = [1, 3, 10, 2, 42, 2, 3, 4, 3]
+
+L3 = [x1 + x2 for (x1, x2) in list(zip(L1, L2)) if x1 > 10 and x2 < 5]
+
+print(L3)
+
+
+
+l1 = ['left', 'up', 'front']
+l2 = ['right', 'down', 'back']
+
+opposites = list(filter(lambda x: (len(x[0]) > 3 and len(x[1]) > 3), zip(l1, l2)))   
+print(opposites)
+
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+
+map_testing = map(lambda s: "Fruit: " + s, lst_check)
+
+print(map_testing)
+
+countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
+
+b_countries = filter(lambda word: "B" in word, countries)
+
+print(b_countries)
